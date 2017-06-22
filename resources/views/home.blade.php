41d81d070798b1a6192a76e8525aab1d7cfa102a
Danny Feliz <dannyfeliz08@gmail.com>
2017-06-22 04:57:35 +0000

    Add input placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Settings</div>
 
                 <div class="panel-body">
-                    <h2>Info</h2>
+                    <p>Configure your <a target="_blank" href="https://github.com">Github</a> username and <a target="_blank" href="https://mctekk.slack.com/apps/A0F7XDUAZ-incoming-webhooks">Slack Webhook</a></p>
 
                         @if (session('message'))
                             <div class="alert alert-success">
@@ -22,12 +22,26 @@
                         <input type="hidden" name="id" value="{{ $token->id }}" >
                         <div class="form-group">
                             <label>Github username</label>
-                            <input type="text" required name="github_username" class="form-control" value="{{ $token->github_username}}">
+                            <input type="text"
+                                   required
+                                   minlength="1"
+                                   name="github_username"
+                                   class="form-control"
+                                   value="{{ $token->github_username}}"
+                                   placeholder="JhonDoe"
+                            >
                         </div>
 
                         <div class="form-group">
-                            <label>Slack token</label>
-                            <input type="text" required name="token" class="form-control" value="{{ $token->token }}">
+                            <label>Slack Webhook</label>
+                            <input type="text"
+                                   required
+                                   name="token"
+                                   class="form-control"
+                                   value="{{ $token->token }}"
+                                   minlength="69"
+                                   placeholder="https://hooks.slack.com/services/XXXXX/XXXXXX/XXXXXX"
+                            >
                         </div>
 
                         <button type="submit" class="btn btn-default">Save</button>
---
+++ resources/views/notifier/notallowed.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.app")
+
+@section("content")
+
+    <h3 style="text-align: center">Human, you're not supposed to be here</h3>
+    <div style="display: block;margin: 0 auto;text-align: center;">
+        <img src="https://emojipedia-us.s3.amazonaws.com/cache/69/c0/69c0b62308243947c2ac885e1cd3d853.png" alt="">
+    </div>
+
+@endsection
