d7d1a21572531a51d4de77fdce2551b16e4068fe
Danny Feliz <danny@mctekk.com>
2017-06-26 04:13:56 -0400

    Fix duplicated notififcation - closes #2

--- app/Http/Controllers/NotifierController.php
+++ app/Http/Controllers/NotifierController.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Danny
- * Date: 15/06/2017
- * Time: 06:37 PM
- */
+* Created by PhpStorm.
+* User: Danny
+* Date: 15/06/2017
+* Time: 06:37 PM
+*/
 
 namespace App\Http\Controllers;
 
@@ -22,27 +22,25 @@ class NotifierController
     use Notifiable;
 
     /**
-     * @param Request $request
-     */
+    * Trigger the notification
+    *
+    * @param Request $request
+    */
     public function index(Request $request)
     {
-        // Convert to array the request payload
         $notification = json_decode($request->toArray()["payload"], true);
 
-        // We only want to trigger the notification if a review is requested
         if ($notification["action"] !== "review_requested") return;
 
-        $reviewers = $notification["pull_request"]["requested_reviewers"];
+        $username = $notification["requested_reviewer"]["login"];
+        $slackToken = SlackToken::where("github_username", $username)->first();
 
-        // Notify each user marked in the reviewer list
-        foreach ($reviewers as $reviewer) {
-            $currentUser = SlackToken::where("github_username", $reviewer["login"])->first();
-            if ($currentUser) {
-                $user = User::where("id", $currentUser->user_id)->first();
-                $user->notify(new ReviewerNotifier($notification));
-            }
+        if ($slackToken) {
+            $user = User::where("id", $slackToken->user_id)->first();
+            $user->notify(new ReviewerNotifier($notification));
         }
     }
+
 
     public function noAllowed()
     {
