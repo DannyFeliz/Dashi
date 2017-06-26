005c4a560ff7b18d4e031e92e1b6c909433c6dbd
Danny Feliz <danny@mctekk.com>
2017-06-26 04:14:58 -0400

    Add the number of commits in the notification - closes #4

--- app/Notifications/ReviewerNotifier.php
+++ app/Notifications/ReviewerNotifier.php
@@ -19,9 +19,9 @@ class ReviewerNotifier extends Notification
     public $client;
 
     /**
-     * Create a new notification instance.
-     * @param Request $notification
-     */
+    * Create a new notification instance.
+    * @param Request $notification
+    */
     public function __construct($notification)
     {
         $this->client = new Client();
@@ -29,64 +29,59 @@ class ReviewerNotifier extends Notification
     }
 
     /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed $notifiable
-     * @return array
-     */
+    * Get the notification's delivery channels.
+    *
+    * @param  mixed $notifiable
+    * @return array
+    */
     public function via($notifiable)
     {
         return ['slack'];
     }
 
     /**
-     * Get the pull request commits list in markdown format
-     *
-     * @return array
-     */
-    public function getCommits()
+    * Get the pull request commits count
+    *
+    * @return array
+    */
+    public function getCommitsCount()
     {
         $commitsUrl = $this->notification["pull_request"]["commits_url"];
         $commits = json_decode($this->client->get($commitsUrl)
-            ->getBody()
-            ->getContents(),
-            true);
+        ->getBody()
+        ->getContents(),
+        true);
 
-        $commitsList = [];
-        foreach ($commits as $commit) {
-            $commitsList[] = '- [' . $commit['commit']['message'] . '](' . $commit['html_url'] . ')';
-        }
-
-        return $commitsList;
+        return count($commits);
     }
 
-     public function toSlack($notifiable)                                                                                                
-     {                                                                                                                                   
+    public function toSlack($notifiable)
+    {
 
-         $notification = $this->notification;                                                                                            
+        $notification = $this->notification;
 
 
-         $user = json_decode($this->client->get($notification["sender"]["url"])                                                          
-             ->getBody()                                                                                                                 
-             ->getContents(),                                                                                                            
-             true);                                                                                                                      
+        $user = json_decode($this->client->get($notification["sender"]["url"])
+        ->getBody()
+        ->getContents(),
+        true);
 
-         $name = $user['name'] ? $user['name'] : $notification["sender"]["login"];                                                       
+        $name = $user['name'] ? $user['name'] : $notification["sender"]["login"];
 
-         return (new SlackMessage)                                                                                                       
-             ->from("Dashi")                                                                                                             
-             ->image("http://icons.iconarchive.com/icons/thehoth/seo/256/seo-web-code-icon.png")                                         
-             ->success()                                                                                                                 
-             ->content(":point_right: ¡{$name} needs you to make him a Code Review to his changes!")                                     
-             ->attachment(function ($attachment) use ($notification) {                                                                   
-                 $attachment->title("Pull Request: " . $notification["pull_request"]["title"], $notification["pull_request"]["html_url"])
-                     ->content("¡Make sure everything is in order before approving the Pull Request!")                                   
-                     ->markdown(["Commits", "text"])                                                                                     
-                     ->fields([                                                                                                          
-                         "User" => $notification["sender"]["login"],                                                                     
-                         "File(s) changed" => $notification["pull_request"]["changed_files"]                                                
-                     ]);                                                                                                                 
-             });                                                                                                                         
-     }                                                                                                                                   
+        return (new SlackMessage)
+        ->from("Dashi")
+        ->image("http://icons.iconarchive.com/icons/thehoth/seo/256/seo-web-code-icon.png")
+        ->success()
+        ->content(":eyes: ¡{$name} needs you to make him a Code Review to his changes!")
+        ->attachment(function ($attachment) use ($notification) {
+            $attachment->title($notification["pull_request"]["title"], $notification["pull_request"]["html_url"])
+            ->content(":sleuth_or_spy: ¡Make sure everything is in order before approving the Pull Request!")
+            ->fields([
+            "User" => $notification["sender"]["login"],
+            "Commit(s)" => $this->getCommitsCount(),
+            "File(s) changed" => $notification["pull_request"]["changed_files"]
+            ]);
+        });
+    }
 
 }
