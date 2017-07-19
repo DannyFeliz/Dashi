8e618a58952cc167659a381a7d837297d8f0483d
Danny Feliz <danny@mctekk.com>
2017-07-19 05:03:06 -0400

    Change routes order

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/save', 'HomeController@save');
 Route::get("/notifier", "NotifierController@noAllowed");
 Route::post("/notifier", "NotifierController@index");
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/save', 'HomeController@save');
