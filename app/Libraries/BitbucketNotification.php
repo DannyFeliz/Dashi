3d16863d24d93391a21b28d2789963e62502572c
Danny Feliz <danny@mctekk.com>
2017-08-07 02:43:33 -0400

    Add Github & Bitbucket libraries

---
+++ app/Libraries/BitbucketNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Libraries;
+
+
+use App\Notifications\RequestReview;
+use App\SlackToken;
+use App\User;
+
+class BitbucketNotification
+{
+    public $notification;
+
+
+    /**
+     * GithubNotification constructor.
+     * @param $notification
+     */
+    public function __construct($notification)
+    {
+        $this->notification = $notification;
+    }
+
+
+    public function run()
+    {
+        $reviewers = $this->notification["pullrequest"]["reviewers"];
+        foreach ($reviewers as $reviewer) {
+            $this->notify($reviewer["username"]);
+        }
+    }
+
+
+    /**
+     * Trigger the notification
+     *
+     * @param string $username
+     */
+    public function notify($username)
+    {
+        $slackToken = SlackToken::where("bitbucket_username", $username)->first();
+        if ($slackToken) {
+            $user = User::where("id", $slackToken->user_id)->first();
+            $user->notify(new RequestReview($this->requestReviewData()));
+        }
+    }
+
+    public function requestReviewData()
+    {
+        return [
+            "username" => $this->notification["pullrequest"]["author"]["username"],
+            "title" => $this->notification["pullrequest"]["title"],
+            "url" =>  $this->notification["pullrequest"]["links"]["html"]["href"],
+            "repository" => $this->notification["pullrequest"]["destination"]["repository"]["name"],
+            "from" => "Bitbucket"
+        ];
+    }
+}
