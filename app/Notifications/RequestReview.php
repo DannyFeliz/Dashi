34d4d4135992efd8525e1b116998cb498bc19e3f
Danny Feliz <dannyfeliz08@gmail.com>
2017-08-07 11:10:23 -0400

    Fix typo in notification content

--- app/Notifications/RequestReview.php
+++ app/Notifications/RequestReview.php
@@ -44,7 +44,7 @@ class RequestReview extends Notification
             ->from(env("APP_NAME"))
             ->image(env("APP_URL") . "/img/dashi-success.png")
             ->success()
-            ->content(":microscope: *{$notification['username']}* needs you to make a `Code Review` to this changes.")
+            ->content(":microscope: *{$notification['username']}* needs you to make a `Code Review` to these changes.")
             ->attachment(function ($attachment) use ($notification) {
                 $attachment->title($notification["title"], $notification["url"])
                            ->content(":sleuth_or_spy: Make sure everything is in order before approve the Pull Request.")
