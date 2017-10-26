34e94352c85a773f3919d0b89072d7c514935e09
Esteban De la Rosa <gabbanaesteban@gmail.com>
2017-10-26 01:26:20 -0400

    Add return type to functions

--- app/Libraries/Parser/ParserInterface.php
+++ app/Libraries/Parser/ParserInterface.php
@@ -2,19 +2,21 @@
 
 namespace App\Libraries\Parser;
 
+use App\Libraries\Slack\SlackAttachment;
+
 interface ParserInterface
 {
     public function __construct(array $request);
 
     public function parse();
 
-    public function getEvent();
+    public function getEvent(): string;
 
-    public function getAttachment();
+    public function getAttachment(): SlackAttachment;
 
-    public function getRawRequest();
+    public function getRawRequest(): array;
 
     public function buildSlackAttachment();
 
-    public function getSuscribers();
+    public function getSuscribers(): array;
 }
